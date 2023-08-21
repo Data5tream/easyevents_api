3b07db85d17f06c2ce0f047a9b8e1398a9112981
Data5tream <6423657+Data5tream@users.noreply.github.com>
2023-08-21 16:08:58 +0200

    add event list endpoint

--- core/urls.py
+++ core/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('profile', views.ProfileView.as_view(), name='profile')
+    path('profile', views.ProfileView.as_view(), name='profile'),
+    path('events', views.EventList.as_view(), name='event_list')
 ]
--- core/views.py
+++ core/views.py
@@ -1,14 +1,25 @@
 from rest_framework.response import Response
 from rest_framework.views import APIView
+from rest_framework import generics, permissions
 
+from .models import Event
 from .permissions import IsOwnerOrAdmin
-from .serializers import UserSerializer
+from .serializers import UserSerializer, EventSerializer
 
 
 class ProfileView(APIView):
     serializer_class = UserSerializer
-    permission_classes = [IsOwnerOrAdmin]
+    permission_classes = [permissions.IsAuthenticated, IsOwnerOrAdmin]
 
     def get(self, request):
         serializer = UserSerializer(request.user)
         return Response(serializer.data)
+
+
+class EventList(generics.ListAPIView):
+    queryset = Event.objects.all()
+    serializer_class = EventSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        return Event.objects.filter(creator=self.request.user)
