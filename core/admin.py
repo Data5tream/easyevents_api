924c8b6f3b02988314ab17593edb01fc3b95449e
Data5tream <6423657+Data5tream@users.noreply.github.com>
2023-08-25 16:21:18 +0200

    use textarea for event description in admin page

--- core/admin.py
+++ core/admin.py
@@ -1,7 +1,22 @@
 from django.contrib import admin
+from django.forms import widgets, ModelForm
+
 from .models import User, Event, EventUpdate
 
-# Register your models here.
+
+class EventAdminForm(ModelForm):
+    class Meta:
+        model = Event
+        fields = '__all__'
+        widgets = {
+            'description': widgets.Textarea
+        }
+
+
+@admin.register(Event)
+class EventAdmin(admin.ModelAdmin):
+    form = EventAdminForm
+
+
 admin.site.register(User)
-admin.site.register(Event)
 admin.site.register(EventUpdate)
