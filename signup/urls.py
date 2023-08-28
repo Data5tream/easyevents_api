2c843f09cb98f5f17a41ec5a7c2b86c9f65232a2
Data5tream <6423657+Data5tream@users.noreply.github.com>
2023-08-28 16:08:24 +0200

    cleanup signup urls

--- signup/urls.py
+++ signup/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from django.contrib.auth import views as auth_views
 
 from . import views
 
@@ -7,5 +8,4 @@ urlpatterns = [
     path('login', auth_views.LoginView.as_view(template_name='signup/login.html'), name='login'),
     path('logout', auth_views.LogoutView.as_view(template_name='signup/logout.html'), name='logout'),
     path('signup/<int:pk>/<str:title>', views.SignupView.as_view(), name='signup_view'),
-    path('logout', views.EventsLogoutView.as_view(), name='logout'),
 ]
